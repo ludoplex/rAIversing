f9b51ec9791e11d2e91f8bc3a0709e3e269e98db
MrMatch246 <s8daerce@stud.uni-saarland.de>
2023-06-14 19:56:52 +0200

    implemented Median

--- modules/rAIversing/evaluator/DefaultEvaluator.py
+++ modules/rAIversing/evaluator/DefaultEvaluator.py
@@ -1,4 +1,5 @@
 import os
+import statistics
 
 from rich.console import Console, CONSOLE_SVG_FORMAT
 from rich.table import Table, Column
@@ -89,7 +90,7 @@ class DefaultEvaluator(EvaluatorInterface):
                     entrypoint = original[orig_name]["entrypoint"]
                 except KeyError:
                     if orig_name.split("_")[-1] == pred_name.split("_")[-1]:
-                        entrypoint = "0x"+(orig_name.split("_")[-1])
+                        entrypoint = "0x" + (orig_name.split("_")[-1])
                     else:
                         entrypoint = find_entrypoint(original, orig_name)
                 score = self.calculator(orig_name, pred_name, entrypoint)
@@ -100,9 +101,6 @@ class DefaultEvaluator(EvaluatorInterface):
     def insert_result(self, run_path, result, group):
         model_name, source_dir_name, run, binary = split_run_path(run_path)
         self.results[model_name][source_dir_name][run][binary][group] = result
-
-    def get_results(self, model_name, source_dir_name, run, binary):
-        return self.results[model_name][source_dir_name][run][binary]
 
     def collect_cumulative_results(self):
         for ai_module in self.ai_modules:
@@ -118,23 +116,40 @@ class DefaultEvaluator(EvaluatorInterface):
             for run in range(1, self.runs + 1):
                 for group, layer in self.results[model][source_dir][run][binary][comp_type].items():
                     if group not in self.results[model][source_dir][0][binary][comp_type].keys():
-                        self.results[model][source_dir][0][binary][comp_type][group] = {"score": 0, "count": 0}
-                    self.results[model][source_dir][0][binary][comp_type][group]["score"] += layer["score"]
-                    self.results[model][source_dir][0][binary][comp_type][group]["count"] += layer["count"]
+                        self.results[model][source_dir][0][binary][comp_type][group] = {"scores": [], "counts": []}
+                    self.results[model][source_dir][0][binary][comp_type][group]["scores"].append(layer["score"])
+                    self.results[model][source_dir][0][binary][comp_type][group]["counts"].append(layer["count"])
 
             for group, layer in self.results[model][source_dir][0][binary][comp_type].items():
-                self.results[model][source_dir][0][binary][comp_type][group]["score"] /= self.runs
-                self.results[model][source_dir][0][binary][comp_type][group]["count"] /= self.runs
+                self.results[model][source_dir][0][binary][comp_type][group]["score"] = sum(
+                    self.results[model][source_dir][0][binary][comp_type][group]["scores"]) / self.runs
+                self.results[model][source_dir][0][binary][comp_type][group]["count"] = sum(
+                    self.results[model][source_dir][0][binary][comp_type][group]["counts"]) / self.runs
 
-    def get_cumulative_results(self, model, source_dir, binary):
-        return self.results[model][source_dir][0][binary]
+    def get_results(self, model_name, source_dir_name, run, binary):
+        return self.results[model_name][source_dir_name][run][binary]
+
+    def get_average_results(self, model_name, source_dir, binary):
+        return self.results[model_name][source_dir][0][binary]
+
+    def get_median_results(self, model_name, source_dir_name, binary):
+        scores = dict(self.results[model_name][source_dir_name][0][binary].copy())
+        output = {}
+        for group_name, group in scores.items():
+            output[group_name] = {}
+            for layer_name, layer in group.items():
+                output[group_name][layer_name] = {}
+                output[group_name][layer_name]["score"] = statistics.median(layer["scores"])
+                output[group_name][layer_name]["count"] = statistics.median(layer["counts"])
+        return output
 
     def display_results(self):
         for ai_module in self.ai_modules:
             model_name = ai_module.get_model_name()
             for source_dir in self.source_dirs:
-                self.create_cumulative_results(model_name, source_dir)
-                self.create_run_results(model_name, source_dir)
+                self.create_median_results(model_name, source_dir)
+                self.create_average_results(model_name, source_dir)
+                # self.create_run_results(model_name, source_dir)
 
     def create_run_results(self, model_name, source_dir):
         source_dir_name = os.path.basename(source_dir)
@@ -147,24 +162,41 @@ class DefaultEvaluator(EvaluatorInterface):
                 self.fill_table(table, scores, binary)
             export_console = Console(record=True, width=160)
             export_console.print(table)
-            run_path = make_run_path(model_name, source_dir, run,"")
-            export_console.save_svg(os.path.join(run_path, f"Eval_{model_name}_{source_dir_name}_run_{run}.svg"), clear=False, title="",
+            run_path = make_run_path(model_name, source_dir, run, "")
+            export_console.save_svg(os.path.join(run_path, f"Eval_{model_name}_{source_dir_name}_run_{run}.svg"),
+                                    clear=False, title="",
                                     code_format=CONSOLE_SVG_FORMAT.replace("{chrome}", ""))
 
-    def create_cumulative_results(self, model_name, source_dir):
+    def create_average_results(self, model_name, source_dir):
         source_dir_name = os.path.basename(source_dir)
-        table = self.create_table(f"{model_name} on {source_dir_name} ({self.runs} runs)")
+        table = self.create_table(f"Average {model_name} on {source_dir_name} ({self.runs} runs)")
         usable_binaries = os.listdir(os.path.join(source_dir, "stripped"))
-        # usable_binaries = ["CNC"]  # TODO remove
         for binary in usable_binaries:
-            source_dir_name = os.path.basename(source_dir)
-            scores = self.get_cumulative_results(model_name, source_dir_name, binary)
+            scores = self.get_average_results(model_name, source_dir_name, binary)
             self.fill_table(table, scores, binary)
-        export_console = Console(record=True, width=160)
+        export_console = Console(record=True, width=165)
         export_console.print(table)
         export_path = make_run_path(model_name, source_dir, "", "")
-        export_console.save_svg(os.path.join(export_path, f"Eval_Avg_{model_name}_{source_dir_name}_{self.runs}_runs.svg"), clear=False, title="",
-                                code_format=CONSOLE_SVG_FORMAT.replace("{chrome}", ""))
+        export_console.save_svg(
+            os.path.join(export_path, f"Eval_Avg_{model_name}_{source_dir_name}_{self.runs}_runs.svg"), clear=False,
+            title="",
+            code_format=CONSOLE_SVG_FORMAT.replace("{chrome}", ""))
+
+    def create_median_results(self, model_name, source_dir):
+        source_dir_name = os.path.basename(source_dir)
+        table = self.create_table(f"Median {model_name} on {source_dir_name} ({self.runs} runs)")
+        usable_binaries = os.listdir(os.path.join(source_dir, "stripped"))
+        for binary in usable_binaries:
+            scores = self.get_median_results(model_name, source_dir_name, binary)
+
+            self.fill_table(table, scores, binary)
+        export_console = Console(record=True, width=165)
+        export_console.print(table)
+        export_path = make_run_path(model_name, source_dir, "", "")
+        export_console.save_svg(
+            os.path.join(export_path, f"Eval_Median_{model_name}_{source_dir_name}_{self.runs}_runs.svg"), clear=False,
+            title="",
+            code_format=CONSOLE_SVG_FORMAT.replace("{chrome}", ""))
 
     def create_table(self, title):
         result_table = Table(Column(header="Binary", style="bold bright_yellow on grey23"),
@@ -195,14 +227,17 @@ class DefaultEvaluator(EvaluatorInterface):
 
         score_best = scores["best"]["all"]["score"]
         score_best_hfl = scores["best"]["hfl"]["score"]
+        score_best_lfl = scores["best"]["lfl"]["score"]
         counted_best = scores["best"]["all"]["count"]
 
         score_worst = scores["worst"]["all"]["score"]
         score_worst_hfl = scores["worst"]["hfl"]["score"]
+        score_worst_lfl = scores["worst"]["lfl"]["score"]
         counted_worst = scores["worst"]["all"]["count"]
 
         score_best_vs_pred_direct = scores["best_vs_pred"]["all"]["score"]
         score_best_vs_pred_direct_hfl = scores["best_vs_pred"]["hfl"]["score"]
+        score_best_vs_pred_direct_lfl = scores["best_vs_pred"]["lfl"]["score"]
 
         total_orig = scores["total_count"]["original"]["count"]
         total_pred = scores["total_count"]["predicted"]["count"]
@@ -211,14 +246,20 @@ class DefaultEvaluator(EvaluatorInterface):
         score_best_vs_pred_hfl = score_pred_hfl / score_best_hfl
 
         # self.console.print(f"score_best_hfl: {score_best_hfl}, score_worst_hfl: {score_worst_hfl}, score_pred_hfl: {score_pred_hfl}")
+        # self.console.print(f"score_best_lfl: {score_best_lfl}, score_worst_lfl: {score_worst_lfl}, score_pred_lfl: {score_pred_lfl}")
         score_rpd_hfl = calc_relative_percentage_difference(score_best_hfl, score_worst_hfl, score_pred_hfl)
-
+        score_rdp_lfl = calc_relative_percentage_difference(score_best_lfl, score_worst_lfl, score_pred_lfl)
         score_rpd = calc_relative_percentage_difference(score_best, score_worst, score_pred)
 
-        table.add_row(binary, f"{score_pred * 100:.2f}%", f"{score_pred_hfl * 100:.2f}%",
-                      f"{score_pred_lfl * 100:.2f}%", f"{score_best_hfl * 100:.2f}%", f"{score_worst_hfl * 100:.2f}%",
+        table.add_row(binary, f"{score_pred * 100:.2f}%",
+                      f"{score_pred_hfl * 100:.2f}%",
+                      f"{score_pred_lfl * 100:.2f}%",
+                      f"{score_best_hfl * 100:.2f}%",
+                      f"{score_worst_hfl * 100:.2f}%",
                       f"{score_best_vs_pred * 100:.1f}%|{score_best_vs_pred_hfl * 100:.1f}%",
                       f"{score_best_vs_pred_direct * 100:.2f}%|{score_best_vs_pred_direct_hfl * 100:.2f}%",
-                      f"{score_rpd :.1f}%|{score_rpd_hfl :.1f}%",
-                      f"{total_orig}[bold magenta1]|[/bold magenta1]{total_pred}", f"{counted_pred}", f"{counted_best}",
-                      f"{counted_worst}")
+                      f"{score_rpd :.1f}%|{score_rpd_hfl :.1f}%|{score_rdp_lfl :.1f}%",
+                      f"{total_orig:.0f}[bold magenta1]|[/bold magenta1]{total_pred:.0f}",
+                      f"{counted_pred:.0f}",
+                      f"{counted_best:.0f}",
+                      f"{counted_worst:.0f}")
