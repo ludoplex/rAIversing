7c6ad65e08a4c5f2ffed27f2b2640d7a33175a47
MrMatch246 <s8daerce@stud.uni-saarland.de>
2023-06-22 14:33:49 +0200

    fix: added reduced binary set path

--- modules/rAIversing/pathing/__init__.py
+++ modules/rAIversing/pathing/__init__.py
@@ -9,6 +9,7 @@ TESTING_ROOT = os.path.join(REPO_ROOT, "testing")
 SAMPLES_ROOT = os.path.join(TESTING_ROOT, "samples")
 BINARIES_ROOT = os.path.join(SAMPLES_ROOT, "binaries")
 P2IM_BINS_ROOT = os.path.join(BINARIES_ROOT, "p2im")
+P2IM_RED_BINS_ROOT = os.path.join(BINARIES_ROOT, "p2im_reduced")
 P2IM_ORIG_BINS = os.path.join(P2IM_BINS_ROOT, "original")
 P2IM_STRIPPED_BINS = os.path.join(P2IM_BINS_ROOT, "stripped")
 P2IM_NO_PROP_BINS = os.path.join(P2IM_BINS_ROOT, "no_propagation")
@@ -17,7 +18,6 @@ GHIDRA_SCRIPTS = os.path.join(GHIDRA_ROOT, "ghidra_scripts")
 AI_MODULES_ROOT = os.path.join(MODULES_ROOT, "rAIversing", "AI_modules")
 DEFAULT_API_KEY_PATH = os.path.join(AI_MODULES_ROOT, "openAI_core", "api_key.txt")
 
-#TODO: fill in the following to match your ghidra installation
+# TODO: fill in the following to match your ghidra installation
 GHIDRA_INSTALL_DIR = os.path.join(GHIDRA_ROOT, "ghidra_10.2.2_PUBLIC")
-GHIDRA_HEADLESS_BINARY = os.path.join(GHIDRA_INSTALL_DIR, "support","analyzeHeadless")
-
+GHIDRA_HEADLESS_BINARY = os.path.join(GHIDRA_INSTALL_DIR, "support", "analyzeHeadless")
