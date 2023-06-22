0050688556234bcb67d1b789b827131dcf4b8206
MrMatch246 <s8daerce@stud.uni-saarland.de>
2023-06-22 16:47:17 +0200

    removed unnecessary interface function

--- modules/rAIversing/AI_modules/__init__.py
+++ modules/rAIversing/AI_modules/__init__.py
@@ -1,10 +1,6 @@
 class AiModuleInterface:
 
 
-    def prompt(self, prompt):  # type: (str) -> str
-        """Prompts the model and returns the result"""
-        pass
-
     def prompt_with_renaming(self, input_code, retries):  # type: (str,int) -> (str, dict)
         """Prompts the model and returns the resulting code and a dict of renamed Names"""
         pass
