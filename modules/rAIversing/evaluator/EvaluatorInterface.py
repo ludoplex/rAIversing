68209160a357d3bf68eacc0d05b782e7f8f4acb9
MrMatch246 <s8daerce@stud.uni-saarland.de>
2023-06-28 17:04:15 +0200

    feat: added pool_size

--- modules/rAIversing/evaluator/EvaluatorInterface.py
+++ modules/rAIversing/evaluator/EvaluatorInterface.py
@@ -1,9 +1,10 @@
 class EvaluatorInterface:
 
-    def __init__(self,ai_modules,source_dirs,runs=1):
+    def __init__(self,ai_modules,source_dirs,runs=1,pool_size=1):
         self.ai_modules = ai_modules
         self.source_dirs = source_dirs
         self.runs = runs
+        self.pool_size = pool_size
 
     def evaluate(self):
         """TODO: Docstring for evaluate."""
